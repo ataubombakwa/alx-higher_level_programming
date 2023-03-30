3718e8c32346bb23f7a5d253ad5084eea49f7e2c
ataubombakwa <ataubsm@gmail.com>
2023-03-30 17:56:18 +0100

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = "".join([char for char in my_string if char.upper() != "C"])
-    return (new_string)
-print(no_c("No more C. Come and Code along with me"))
+    return my_string.translate({ord(i): None for i in 'cC'})
